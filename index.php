6caff0ba2d51d8e274c31a96e18935559021e86d
Vladislavs <Vladikuk948@gmail.com>
2023-01-01 21:05:24 +0200

    index clear

--- index.php
+++ index.php
@@ -14,24 +14,18 @@ $telegram = new SendTelegram();
 
 $request = new Request($_GET, $_POST);
 
-echo Converter::ConvertCurrency(252);
-//
-//$get = $request->getQueryParams();
-//if (array_key_exists('pay', $get)) {
-//    echo  Converter::ConvertCurrency($get['pay']);
-//
-//} else {
-//    echo "in query params doesnt have pay param";
-//}
-//$converter = new Converter();
-//$re = $converter->GetCurrency();
-//echo "\ngood";
-//if (is_numeric($get)) {
-//    $data = "New payment " . $get . " USD!!!";
-//    echo $get;
-//    try {
-//        $telegram->send($data);
-//    } catch (Exception $exception) {
-//        throw new Exception();
-//    }
-//}
+
+
+$get = $request->getQueryParams();
+if (array_key_exists('pay', $get)) {
+    $money =  Converter::ConvertCurrency($get['pay']);
+    $data = "New payment " . $money . " USD!!!";
+    echo $money;
+    try {
+        $telegram->send($data);
+    } catch (Exception $exception) {
+        throw new Exception();
+    }
+} else {
+    echo "in query params doesnt have pay param";
+}
